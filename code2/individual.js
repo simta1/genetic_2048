af5679955d49c99aa15b7c87458b74821faa19d9
simta1 <133639447+simta1@users.noreply.github.com>
2024-04-10 03:33:05 +0900

    update individual.js

--- code2/individual.js
+++ code2/individual.js
@@ -4,13 +4,12 @@ class Individual {
         for (let i = 0; i < size * size; i++) this.chromosome.push(Math.random());
 
         this.game = new Game();
-        print(this.game.curBoard);
         this.agent = new Agent(this.chromosome, agentPedictDepth, agentRepeatLimit);
     }
 
     run() {
-        this.game.run();
         this.agent.run(this.game);
+        this.game.run();
     }
     
     show() {
